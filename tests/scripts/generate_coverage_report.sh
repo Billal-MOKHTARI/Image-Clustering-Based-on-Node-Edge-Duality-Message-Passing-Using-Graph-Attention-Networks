a399d7ad135e74f4e3554f952f1878fea0618f0f
Billal MOKHTARI <100322125+Billal-MOKHTARI@users.noreply.github.com>
2024-03-28 23:56:09 +0000

    Add coverage configuration and script for generating coverage report

---
+++ tests/scripts/generate_coverage_report.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Set the necessary environment variables
+export REPORT_DIR="/workspaces/Image-Clustering-Based-on-Node-Edge-Duality-Message-Passing-Using-Graph-Attention-Networks/tests/reports"
+export GENERATED_REPORT_DIR="/workspaces/Image-Clustering-Based-on-Node-Edge-Duality-Message-Passing-Using-Graph-Attention-Networks/htmlcov"
+
+
+
+python -m pytest --cov=. --cov-report html
+
+mv "${GENERATED_REPORT_DIR}" "${REPORT_DIR}" 
+
+
+
+
+
+echo "Coverage report generated successfully!"
