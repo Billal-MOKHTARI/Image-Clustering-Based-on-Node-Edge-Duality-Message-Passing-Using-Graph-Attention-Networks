23b0e8ed059934720144d4c5e6a152be2def4071
Billal-MOKHTARI <Billal.MOKHTARI@etu.uca.fr>
2024-03-31 15:54:48 +0200

    Refactor PyModelManager class methods

--- src/py_model_manager.py
+++ src/py_model_manager.py
@@ -12,38 +12,36 @@ class PyModelManager:
         self.list_layers = list()
     
     def get_named_layers(self):
-
-        self.dict_layers = dict(self.model.named_children())
-        return self.dict_layers
-
-    def get_list_layers(self):
-
-        # Get the layers of the pre-trained model
-        self.list_layers = list(self.model.children())
-        return self.list_layers
-
-    def get_layer_by_name(self, layer_name, layer_index=None):
+        def get_layers_recursive(model_children: dict):
+            for name, layer in model_children.items():
+                if dict(layer.named_children()) != {}:
+                    model_children[name] = get_layers_recursive(dict(layer.named_children()))
+                else:
+                    model_children[name] = layer
+            self.named_layers = model_children
+            return self.named_layers
         
-        
-        if layer_index is None:
-            return self.get_named_layers()[layer_name]
-        else:
-            return self.get_named_layers()[layer_name][layer_index]
+        return get_layers_recursive(dict(self.model.named_children()))
 
-    def get_layer_by_index(self, block_index, layer_index=None):
-        if layer_index is None:
-            return self.get_list_layers()[block_index]
+    def get_list_layers(self, data):    
+        pass
+
+    def get_layer(self, indexes):
+        trace = ['self.model']
+        for index in indexes[:-1]:
+            
+            if isinstance(index, int):
+                trace.append(f'[{index}]')
+            else:
+                trace.append(f'.{index}')
+        
+        if isinstance(indexes[-1], int):
+            layers = eval(''.join(trace))
+            return layers[indexes[-1]]
         else:
-            return self.get_list_layers()[block_index][layer_index]
-    
-    def delete_layer_by_name(self, layer_name, layer_index=None):
-        if layer_index is None:
-            delattr(self.model, layer_name)
-        else:
-            layers = self.get_named_layers()[layer_name]
-            del layers[layer_index]
-    
-    def delete_layer_recursive(self, indexes):
+            return getattr(eval(''.join(trace)), indexes[-1])
+
+    def delete_layer(self, indexes):
         trace = ['self.model']
         for index in indexes[:-1]:
             
@@ -57,14 +55,17 @@ class PyModelManager:
             del layers[indexes[-1]]
         else:
             delattr(eval(''.join(trace)), indexes[-1])
+
+    def get_layer_by_index(self, block_index, layer_index=None):
+        if layer_index is None:
+            return self.get_list_layers()[block_index]
+        else:
+            return self.get_list_layers()[block_index][layer_index]
+    
+
             
     
-    def delete_layer_by_index(self, block_index, layer_index=None):
-        if layer_index is None:
-            del self.list_layers[block_index]
-        else:
-            del self.list_layers[block_index][layer_index]        
-    
+    # Get properties of the layer
     def get_output_size(self, layer):
         assert hasattr(layer, 'out_features'), f'{layer} does not have the attribute out_features'
         return layer.out_features
@@ -72,10 +73,36 @@ class PyModelManager:
     def get_input_size(self, layer):
         assert hasattr(layer, 'in_features'), f'{layer} does not have the attribute in_features'
         return layer.in_features
+    def get_attribute(self, layer, attribute):
+        assert hasattr(layer, attribute), f'{layer} does not have the attribute {attribute}'
+        return getattr(layer, attribute)
+    
+    # Search for a layer in the model by property
+    def search_layer(self, property, value):
+        def dfs(self, model, property, value, layers, indexes, tmp=[]):
+            for name, layer in model.named_children():
+                print(dict(layer.named_children()))
+
+                if hasattr(layer, property) and self.get_attribute(layer, property) == value:
+                    layers.append(layer)
+                    indexes.append(tmp)
+                    tmp = []
+                else:
+                    tmp.append(name)
+                    dfs(self, layer, property, value, layers, indexes, tmp)
+
+        layers = []
+        indexes = []
+
+        dfs(self, self.model, property, value, layers, indexes)
+        return layers, indexes
+    
+    def delete_by_attribute(self, property, value):
+        pass
     
 vgg = models.vgg16(pretrained=True)
 
 model = PyModelManager(vgg)
-model.delete_layer_recursive(['classifier', -1])
-print(model.get_named_layers())
+# model.delete_layer_recursive(['classifier', -1])
+print(model.search_layer('out_features', 4096))
 # print(vgg.classifier[0])
