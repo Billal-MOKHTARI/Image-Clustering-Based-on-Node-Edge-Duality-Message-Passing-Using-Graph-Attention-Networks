3b37c1bca3eeee023b27154a619f28ebe98ef1da
Billal-MOKHTARI <Billal.MOKHTARI@etu.uca.fr>
2024-04-13 17:17:00 +0200

    Refactor LinearDot2D and MessagePassing classes in node_block_components folder

--- models/node_block_components/linear_dot_2d.py
+++ models/node_block_components/linear_dot_2d.py
@@ -7,49 +7,47 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'
 import src.utils
 
 class LinearDot2D(nn.Module):
-    def __init__(self, depth, in_features, out_features, **kwargs):
+    def __init__(self, depth, n_features, **kwargs):
         super(LinearDot2D, self).__init__()
         self.list_linear = []
         self.depth = depth
 
 
         for i in range(depth):
-            self.list_linear.append(nn.Linear(in_features, out_features))
+            self.list_linear.append(nn.Linear(n_features, n_features))
    
 
     def forward(self, x):
         assert len(x.shape) == 3, "The input tensor should be a 3D tensor"
         assert len(x) == self.depth, "The depth of the tensor should be equal to the depth of the model"
 
-        channels = src.utils.extract_channels(x)
+
 
         list_product = []
-        for channel, layer in zip(channels, self.list_linear):
-   
-            list_product.append(layer(torch.tensor(np.array(channel))))
+        for channel, layer in zip(x, self.list_linear):
+            list_product.append(layer(channel))
             
-
-        return list_product
+        return torch.stack(list_product)
     
-model = LinearDot2D(depth=3, in_features=5, out_features=3)
+# model = LinearDot2D(depth=3, n_features=5)
 
-mat1 = np.array([[1, 1, 0, 1, 1], 
-                [1, 1, 1, 0, 0], 
-                [0, 1, 1, 0, 1], 
-                [1, 0, 0, 1, 0],
-                [1, 0, 1, 0, 1]])
+# mat1 = np.array([[1, 1, 0, 1, 1], 
+#                 [1, 1, 1, 0, 0], 
+#                 [0, 1, 1, 0, 1], 
+#                 [1, 0, 0, 1, 0],
+#                 [1, 0, 1, 0, 1]])
 
-mat2 = np.array([[1, 0, 1, 1, 1], 
-                [0, 1, 1, 0, 1], 
-                [1, 1, 1, 1, 1], 
-                [1, 0, 1, 1, 0],
-                [1, 1, 1, 0, 1]])
+# mat2 = np.array([[1, 0, 1, 1, 1], 
+#                 [0, 1, 1, 0, 1], 
+#                 [1, 1, 1, 1, 1], 
+#                 [1, 0, 1, 1, 0],
+#                 [1, 1, 1, 0, 1]])
 
-mat3 = np.array([[1, 1, 0, 0, 1], 
-                [1, 1, 0, 1, 0], 
-                [0, 0, 1, 0, 1], 
-                [0, 1, 0, 1, 0],
-                [1, 0, 1, 0, 1]])
+# mat3 = np.array([[1, 1, 0, 0, 1], 
+#                 [1, 1, 0, 1, 0], 
+#                 [0, 0, 1, 0, 1], 
+#                 [0, 1, 0, 1, 0],
+#                 [1, 0, 1, 0, 1]])
 
-mat = np.array([mat1, mat2, mat3])
-print(model(torch.tensor(mat, dtype=torch.float32)))
+# mat = torch.tensor(np.array([mat1, mat2, mat3], dtype=np.float32))
+# print(model(mat).shape)
