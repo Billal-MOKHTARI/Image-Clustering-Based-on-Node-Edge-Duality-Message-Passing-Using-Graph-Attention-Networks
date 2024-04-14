827076b6832871e1831df1908f9b5dd704f06584
Billal-MOKHTARI <Billal.MOKHTARI@etu.uca.fr>
2024-04-14 04:36:58 +0200

    Refactor create_dual_adjacency_tensor function in DualMessagePassing class

--- models/dual_message_passing.py
+++ models/dual_message_passing.py
@@ -9,57 +9,148 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 from src import data_loader
 
 class DualMessagePassing(nn.Module):
+    """
+    >>> DualMessagePassing module performs message passing on both primal and dual graphs.
 
-    def __init__(self, node_graph_order, 
+    >>> Args:
+    >>>     primal_in_features (int): Number of input features for the primal graph.
+    >>>     primal_out_features (int): Number of output features for the primal graph.
+    >>>     primal_index (list): List of indices for the primal graph.
+    >>>     depth (int): Depth of the message passing.
+    >>>     dual_in_features (int): Number of input features for the dual graph.
+    >>>     dual_out_features (int): Number of output features for the dual graph.
+    >>>     dual_index (list): List of indices for the dual graph.
+    >>>     layer_index (int): Index of the layer.
+    >>>     delimiter (str, optional): Delimiter used to split indices. Defaults to "_".
+    >>>     **kwargs: Additional keyword arguments for message passing.
+
+    >>> Attributes:
+    >>>     node_message_passing_args (dict): Additional arguments for node message passing.
+    >>>     edge_message_passing_args (dict): Additional arguments for edge message passing.
+    >>>     node_graph_order (int): Order of the primal graph.
+    >>>     primal_in_features (int): Number of input features for the primal graph.
+    >>>     primal_out_features (int): Number of output features for the primal graph.
+    >>>     primal_index (list): List of indices for the primal graph.
+    >>>
+        dual_graph_order (int): Order of the dual graph.
+        dual_in_features (int): Number of input features for the dual graph.
+        dual_out_features (int): Number of output features for the dual graph.
+        dual_index (list): List of indices for the dual graph.
+        layer_index (int): Index of the layer.
+        delimiter (str): Delimiter used to split indices.
+        node_message_passing_block (MessagePassing): Node message passing block.
+        edge_message_passing_block (MessagePassing): Edge message passing block.
+
+    Methods:
+        create_dual_adjacency_tensor(mapper): Creates the dual adjacency tensor.
+        create_primal_adjacency_tensor(mapper): Creates the primal adjacency tensor.
+        forward(node_x, edge_x, primal_adjacency_tensor, dual_adjacency_tensor): Performs forward pass.
+
+    Returns:
+        dict: Dictionary containing the primal and dual outputs and adjacency tensors. primal and dual outputs and adjacency tensors.
+    
+    >>> Example :
+    >>>     model = DualMessagePassing(
+    >>>                             primal_in_features=3, 
+    >>>                             primal_out_features=7, 
+    >>>                             depth=3,
+    >>>                             primal_index=["n1", "n2", "n3", "n4"],
+    >>>                             dual_in_features=5,
+    >>>                             dual_out_features=3,
+    >>> 
+    >>>                             dual_index=["n1_n2", "n1_n3", "n2_n4"], 
+    >>>                             layer_index=1,
+    >>>                             delimiter="_",
+    >>>                             node_message_passing_args={"batch_norm": {"momentum": 0.1}, "activation": {"layer": nn.ReLU, "args": {}}},
+    >>>                             edge_message_passing_args={"batch_norm": {"momentum": 0.1}, "activation": {"layer": nn.ReLU, "args": {}}}
+    >>>                             )
+
+    >>>     node_x = torch.tensor(np.array([[1, 2, 3], [4, 6, 5], [7, 8, 9], [10, 11, 12]]), dtype=torch.float32)
+    >>>     edge_x = torch.tensor(np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]]), dtype=torch.float32)
+    >>>     primal_adjacency_tensor = torch.tensor(np.array([[[1, 1, 1, 0], 
+    >>>                                                     [1, 1, 0, 0], 
+    >>>                                                     [1, 0, 1, 0], 
+    >>>                                                     [0, 0, 0, 1]], 
+    >>>                                                     [[1, 1, 1, 0], 
+    >>>                                                     [1, 1, 0, 1], 
+    >>>                                                     [1, 0, 1, 0], 
+    >>>                                                     [0, 1, 0, 1]], 
+    >>>                                                     [[1, 1, 0, 0], 
+    >>>                                                     [1, 1, 0, 1], 
+    >>>                                                     [0, 0, 1, 0], 
+    >>>                                                     [0, 1, 0, 1]]]), dtype=torch.float32)
+    >>>     dual_adjacency_tensor = torch.tensor(np.array([[[1, 2, 3], 
+    >>>                                                     [2, 1, 1], 
+    >>>                                                     [3, 1, 0]], 
+    >>>                                                 [[1, 0, 1], 
+    >>>                                                     [0, 1, 0], 
+    >>>                                                     [1, 0, 1]], 
+    >>>                                                 [[1, 0, 1], 
+    >>>                                                     [0, 1, 1], 
+    >>>                                                     [1, 1, 1]]]), dtype=torch.float32)
+    >>>     result = model(node_x, edge_x, primal_adjacency_tensor, dual_adjacency_tensor)
+    >>>     print(result)
+
+    """
+
+    def __init__(self,
                 primal_in_features, 
                 primal_out_features, 
-                primal_depth,
                 primal_index,
-                dual_graph_order,
+                depth,
                 dual_in_features,
                 dual_out_features,
-                dual_depth,
                 dual_index, 
                 layer_index,
-
+                delimiter="_",
                 **kwargs):
-        super(DualMessagePassing).__init__()
+        super(DualMessagePassing, self).__init__()
         self.node_message_passing_args = kwargs.get("node_message_passing_args", {})
         self.edge_message_passing_args = kwargs.get("edge_message_passing_args", {})
 
-        self.node_graph_order = node_graph_order
+        self.node_graph_order = len(primal_index)
         self.primal_in_features = primal_in_features
         self.primal_out_features = primal_out_features
-        self.primal_depth = primal_depth
         self.primal_index = primal_index
-        self.dual_graph_order = dual_graph_order
+        self.depth = depth
+        self.dual_graph_order = len(dual_index)
         self.dual_in_features = dual_in_features
         self.dual_out_features = dual_out_features
-        self.dual_depth = dual_depth
         self.dual_index = dual_index
+ 
         self.layer_index = layer_index
+        self.delimiter = delimiter
 
         self.node_message_passing_block = MessagePassing(graph_order=self.node_graph_order, 
                                                    in_features=self.primal_in_features, 
                                                    out_features=self.primal_out_features, 
-                                                   depth=self.primal_depth, 
+                                                   depth=self.depth, 
                                                    layer_index=self.layer_index, 
                                                    **self.node_message_passing_args)
         
         self.edge_message_passing_block = MessagePassing(graph_order=self.dual_graph_order, 
                                                    in_features=self.dual_in_features, 
                                                    out_features=self.dual_out_features, 
-                                                   depth=self.dual_depth, 
+                                                   depth=self.primal_in_features, 
                                                    layer_index=self.layer_index, 
                                                    **self.edge_message_passing_args)
 
-    def create_dual_adjacency_tensor(self, delimiter, mapper):
+    def create_dual_adjacency_tensor(self, mapper):
+        """
+        Creates the dual adjacency tensor.
+
+        Args:
+            mapper (pd.DataFrame): DataFrame containing the mapping between indices and values.
+
+        Returns:
+            torch.Tensor: Dual adjacency tensor.
+        """
         tensor = torch.zeros(mapper.shape[1], len(self.dual_index), len(self.dual_index))
 
         for ind1 in self.dual_index:
             for ind2 in self.dual_index:
-                ind1_split = ind1.split(delimiter)
-                ind2_split = ind2.split(delimiter)
+                ind1_split = ind1.split(self.delimiter)
+                ind2_split = ind2.split(self.delimiter)
 
                 intersection = data_loader.intersect_list(ind1_split, ind2_split)
                 if len(intersection) == 1:
@@ -70,31 +161,102 @@ class DualMessagePassing(nn.Module):
 
         return tensor
 
-    def create_primal_adjacency_tensor(self, primal_index, delimiter, mapper):
+    def create_primal_adjacency_tensor(self, mapper):  
+        """
+        Creates the primal adjacency tensor.
 
-        pass
+        Args:
+            mapper (pd.DataFrame): DataFrame containing the mapping between indices and values.
 
-    def forward(self, node_x, edge_x, node_adjacency_tensor, edge_adjacency_tensor):
-        node_output = self.node_message_passing_block(node_x, node_adjacency_tensor)
-        edge_output = self.edge_message_passing_block(edge_x, edge_adjacency_tensor)
+        Returns:
+            torch.Tensor: Primal adjacency tensor.
+        """
+        tensor = torch.zeros(mapper.shape[1], len(self.primal_index), len(self.primal_index))
+        
+        mapper_index = list(mapper.index)
+        for ind in mapper_index:
+            ind_split = ind.split(self.delimiter)
+            ind_row, ind_col = self.primal_index.index(ind_split[0]), self.primal_index.index(ind_split[1])
+
+            tensor[:, ind_row, ind_col] = torch.tensor(mapper.loc[ind])
+            tensor[:, ind_col, ind_row] = torch.tensor(mapper.loc[ind])
+        
+        for i in range(len(self.primal_index)):
+            tensor[:, i, i] = torch.tensor(np.ones(mapper.shape[1]))
+
+        return tensor
+
+    def forward(self, node_x, edge_x, primal_adjacency_tensor, dual_adjacency_tensor):
+        """
+        Performs forward pass.
+
+        Args:
+            node_x (torch.Tensor): Input tensor for the primal graph.
+            edge_x (torch.Tensor): Input tensor for the dual graph.
+            primal_adjacency_tensor (torch.Tensor): Primal adjacency tensor.
+            dual_adjacency_tensor (torch.Tensor): Dual adjacency tensor.
+
+        Returns:
+            dict: Dictionary containing the primal and dual outputs and adjacency tensors.
+        """
+        primal_output = self.node_message_passing_block(node_x, primal_adjacency_tensor)
+        dual_output = self.edge_message_passing_block(edge_x, dual_adjacency_tensor)
+        
+        primal_adjacency_tensor = self.create_primal_adjacency_tensor(pd.DataFrame(dual_output.detach().numpy(), index=self.dual_index))
+        dual_adjacency_tensor = self.create_dual_adjacency_tensor(pd.DataFrame(primal_output.detach().numpy(), index=self.primal_index))
 
 
-        pass
+        result = {
+                    "primal": 
+                            {"nodes": primal_output, 
+                             "adjacency_tensor": primal_adjacency_tensor
+                            },
+                    "dual": 
+                            {
+                            "nodes": dual_output, 
+                             "adjacency_tensor": dual_adjacency_tensor
+                            }
+                }
 
-def create_dual_adjacency_tensor(dual_index, delimiter, mapper):
-    tensor = torch.zeros(mapper.shape[1], len(dual_index), len(dual_index))
+        return result
+    
+model = DualMessagePassing(
+                            primal_in_features=3, 
+                            primal_out_features=7, 
+                            depth=3,
+                            primal_index=["n1", "n2", "n3", "n4"],
+                            dual_in_features=5,
+                            dual_out_features=3,
+    
+                            dual_index=["n1_n2", "n1_n3", "n2_n4"], 
+                            layer_index=1,
+                            delimiter="_",
+                            node_message_passing_args={"batch_norm": {"momentum": 0.1}, "activation": {"layer": nn.ReLU, "args": {}}},
+                            edge_message_passing_args={"batch_norm": {"momentum": 0.1}, "activation": {"layer": nn.ReLU, "args": {}}}
+                            )
 
-    for ind1 in dual_index:
-        for ind2 in dual_index:
-            ind1_split = ind1.split(delimiter)
-            ind2_split = ind2.split(delimiter)
-
-            intersection = data_loader.intersect_list(ind1_split, ind2_split)
-            if len(intersection) == 1:
-                index_row, index_col, value = dual_index.index(ind1), dual_index.index(ind2), mapper.loc[intersection[0]]
-                tensor[:, index_row, index_col] = torch.tensor(value)    
-            elif len(intersection) == 2:
-                tensor[:, dual_index.index(ind1), dual_index.index(ind2)] = torch.tensor(np.ones(mapper.shape[1]))
-    return tensor 
-dual_index = ["a_b", "b_c", "c_a"]
-print(create_dual_adjacency_tensor(dual_index, "_", pd.DataFrame(np.array([[1, 2, 3, 7], [4, 5, 6, 2], [7, 8, 9, 10]]), index=['a', 'b', 'c'])))
+node_x = torch.tensor(np.array([[1, 2, 3], [4, 6, 5], [7, 8, 9], [10, 11, 12]]), dtype=torch.float32)
+edge_x = torch.tensor(np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]]), dtype=torch.float32)
+primal_adjacency_tensor = torch.tensor(np.array([[[1, 1, 1, 0], 
+                                                  [1, 1, 0, 0], 
+                                                  [1, 0, 1, 0], 
+                                                  [0, 0, 0, 1]], 
+                                                 [[1, 1, 1, 0], 
+                                                  [1, 1, 0, 1], 
+                                                  [1, 0, 1, 0], 
+                                                  [0, 1, 0, 1]], 
+                                                 [[1, 1, 0, 0], 
+                                                  [1, 1, 0, 1], 
+                                                  [0, 0, 1, 0], 
+                                                  [0, 1, 0, 1]]]), dtype=torch.float32)
+dual_adjacency_tensor = torch.tensor(np.array([[[1, 2, 3], 
+                                                [2, 1, 1], 
+                                                [3, 1, 0]], 
+                                               [[1, 0, 1], 
+                                                [0, 1, 0], 
+                                                [1, 0, 1]], 
+                                               [[1, 0, 1], 
+                                                [0, 1, 1], 
+                                                [1, 1, 1]]]), dtype=torch.float32)
+result = model(node_x, edge_x, primal_adjacency_tensor, dual_adjacency_tensor)
+print(result)
