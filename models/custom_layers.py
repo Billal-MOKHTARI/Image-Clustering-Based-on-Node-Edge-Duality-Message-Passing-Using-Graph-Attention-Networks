3246825640a814902e7b12a86b8df50bb18f37c2
Billal-MOKHTARI <Billal.MOKHTARI@etu.uca.fr>
2024-04-21 05:08:37 +0200

    Add Decoder class

--- models/custom_layers.py
+++ models/custom_layers.py
@@ -168,12 +168,11 @@ class Decoder2D(nn.Module):
                  layers, 
                  activation, 
                  num_conv_layers, 
-                 k_sizes, 
                  strides, 
-                 
-                 k_size_pool,
+                 padding,
+                 sizes,
                  stride_pool,
-                
+                 padding_pool,
                  dropout_p=0.5,
                  **kwargs):
         super(Decoder2D, self).__init__()
@@ -188,24 +187,22 @@ class Decoder2D(nn.Module):
             
         if length > 1:
             self.mlp_layers.append(nn.Linear(latent_dims[-2], latent_dims[-1]))
-
-        self.decoder_layers.extend(self.deconv2d_block(latent_dims[-1], 
-                                                layers[0], 
-                                                num_conv_layers[0], 
-                                                k_sizes[0], 
-                                                strides[0], 
-                                                k_size_pool[0] if type(k_size_pool) == list else k_size_pool,
-                                                stride_pool[0] if type(stride_pool) == list else stride_pool   
-                                                ))
+        
+        kernel_size = (maths.required_kernel_transpose(1, sizes[0][0], stride=1, padding=0),
+                          maths.required_kernel_transpose(1, sizes[0][1], stride=1, padding=0))
+        self.decoder_layers.append(nn.ConvTranspose2d(latent_dims[-1], layers[0], kernel_size, 1, padding=0))
         
         for i in range(len(layers)-1):
             self.decoder_layers.extend(self.deconv2d_block(layers[i], 
                                                            layers[i+1], 
-                                                           num_conv_layers[i] if type(num_conv_layers) == list else num_conv_layers[i], 
-                                                           k_sizes[i] if type(k_sizes) == list else k_sizes[i], 
-                                                           strides[i] if type(strides) == list else strides[i], 
-                                                           k_size_pool[i] if type(k_size_pool) == list else k_size_pool,
-                                                           stride_pool[i] if type(stride_pool) == list else stride_pool   
+                                                           num_conv_layers[i] if type(num_conv_layers) == list else num_conv_layers, 
+                                                           
+                                                           stride = strides[i] if type(strides) == list else strides, 
+                                                           padding = padding[i] if type(padding) == list else padding,
+                                                           in_size=sizes[i],
+                                                           out_size=sizes[i+1],
+                                                           pool_stride = stride_pool[i] if type(stride_pool) == list else stride_pool,
+                                                            pool_padding = padding_pool[i] if type(padding_pool) == list else padding_pool   
                                                            ))
         
 
@@ -223,27 +220,35 @@ class Decoder2D(nn.Module):
                        in_channels, 
                        out_channels, 
                        num_conv_layers, 
-                       kernel_size, 
                        stride, 
-                       
-                       pool_kernel, 
+                       padding,
+                       in_size,
+                       out_size,
+
                        pool_stride=(2, 2),
                        pool_padding=(0, 0)):
         
         deconv2d_layers = []
-
+        pool_kernel_size = (maths.required_kernel_transpose(in_size[0], out_size[0], stride=pool_stride[0], padding=pool_padding[0]),
+        maths.required_kernel_transpose(in_size[1], out_size[1], stride=pool_stride[1], padding=pool_padding[1]))
+        unpool2d = nn.MaxUnpool2d(pool_kernel_size, pool_stride, pool_padding)
+        deconv2d_layers.append(unpool2d)
         
-        deconv2d_layers.append(nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding=0))
+        # We should keep the input size as it is
+        kernel_size = (maths.required_kernel_transpose(in_size[0], in_size[0], stride=stride[0], padding=padding[0]),
+                       maths.required_kernel_transpose(in_size[1], in_size[1], stride=stride[1], padding=padding[1]))
+        
+        deconv2d_layers.append(nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding=padding))
         deconv2d_layers.append(nn.BatchNorm2d(out_channels))
         deconv2d_layers.append(self.activation)
         
         for _ in range(num_conv_layers-1):
-            deconv2d_layers.append(nn.ConvTranspose2d(out_channels, out_channels, kernel_size, stride, padding=0))
+
+            deconv2d_layers.append(nn.ConvTranspose2d(out_channels, out_channels, kernel_size, stride, padding))
             deconv2d_layers.append(nn.BatchNorm2d(out_channels))
             deconv2d_layers.append(self.activation)
             
-        unpool2d = nn.MaxUnpool2d(pool_kernel, pool_stride, pool_padding)
-        deconv2d_layers.append(unpool2d)
+
 
         
         return deconv2d_layers
@@ -253,16 +258,16 @@ class Decoder2D(nn.Module):
 
         x = x.view(x.size(0), -1, 1, 1)  # Reshape to (batch_size, channels, height, width)
         
+        k = 0
+        
         for i in range(len(self.decoder_layers)):
-            k = 0
             if isinstance(self.decoder_layers[i], nn.MaxUnpool2d):
+                print("ok:", pool_indices[k].shape)
                 x = self.decoder_layers[i](x, pool_indices[k])
                 k += 1
             else:
                 x = self.decoder_layers[i](x)
-      
-        
-        
+            
         return x
 
 layers = [3, 64, 128, 256, 512]
@@ -283,32 +288,30 @@ encoder = Encoder2D(layers=layers,
                   padding_pool=padding,
                   pool_type='max')
 
-in_sizes = [14, 28, 56, 112]
-out_sizes = [28, 56, 112, 224]
-padding = 0
-stride = 2
-conv_transpose_kernels = [maths.required_kernel_transpose(in_size, out_size, stride=stride, padding=padding) for in_size, out_size in zip(in_sizes, out_sizes)]
+
 
 
 decoder = Decoder2D(layers=layers[::-1], 
                   latent_dims=latent_dims[::-1], 
                   activation=nn.ReLU, 
-                  num_conv_layers=[2, 2, 2, 2], 
-                  k_sizes=[3, 3, 3, 3], 
-                  strides=[1, 1, 1, 1], 
-                  k_size_pool=conv_transpose_kernels,
-                  stride_pool=stride,
+                  num_conv_layers=2, 
+                  
+                  strides=(1, 1),
+                  padding=(0, 0), 
+                  sizes=sizes[::-1],
+                  stride_pool=(1, 1),
+                  padding_pool = (0, 0)
                 )
 
 # summary(model, (3, 224, 224))
 x_enc = torch.randn(2, 3, 224, 224)
 x_dec = torch.randn(2, 512)
 y_enc, indices = encoder(x_enc)
-print(y_enc.shape)
+for ind in indices[::-1]:
+    print("index shape : ", ind.shape)
 
-
-# y_dec = decoder(y_enc, indices[::-1])
-# print(y_dec)
+y_dec = decoder(y_enc, indices[::-1])
+print(y_dec)
 # x = torch.randn(2, 4096, 1, 1)
 # layer = nn.ConvTranspose2d(4096, 512, 3, 1) 
 # out = layer(x)
