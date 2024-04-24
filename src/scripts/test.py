7dbdc72e053b3cad27a3948c79f9aefcd8b2f71a
Billal-MOKHTARI <billal.mokhtari@etu.uca.fr>
2024-04-24 20:16:29 +0200

    Refactor data visualization functions and add read-only mode to run creation in test.py

--- src/scripts/test.py
+++ src/scripts/test.py
@@ -3,4 +3,5 @@ from torch_model_manager import TorchModelManager, NeptuneManager
 nm = NeptuneManager(project="Billal-MOKHTARI/Image-Clustering-based-on-Dual-Message-Passing",
                      api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiI0NGRlOTNiZC0zNGZlLTRjNWUtYWEyMC00NzEwOWJkOTRhODgifQ==",
                         run_ids_path="../../configs/run_ids.json")
-run = nm.create_run("data_visualization")
+run = nm.create_run("data_visualization", read_only=True)
+print(run["embeddings"].download(destination="artifacts", progress_bar=True))
