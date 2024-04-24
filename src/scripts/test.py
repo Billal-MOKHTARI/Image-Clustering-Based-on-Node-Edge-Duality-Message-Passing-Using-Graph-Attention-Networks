a50e67e081187e7ce20110493d638e8508b27b2e
Billal-MOKHTARI <Billal.MOKHTARI@etu.uca.fr>
2024-04-25 01:29:13 +0200

    Refactor data visualization functions and add image plotting utility in test.py

--- src/scripts/test.py
+++ src/scripts/test.py
@@ -1,7 +1,8 @@
 from torch_model_manager import TorchModelManager, NeptuneManager
+import os
 
 nm = NeptuneManager(project="Billal-MOKHTARI/Image-Clustering-based-on-Dual-Message-Passing",
                      api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiI0NGRlOTNiZC0zNGZlLTRjNWUtYWEyMC00NzEwOWJkOTRhODgifQ==",
                         run_ids_path="../../configs/run_ids.json")
-run = nm.create_run("data_visualization", read_only=True)
-print(run["embeddings"].download(destination="artifacts", progress_bar=True))
+run = nm.create_run("data_visualization")
+print(run["images/vgg16"].download(progress_bar=True))
