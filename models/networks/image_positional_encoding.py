add188a13dcf9e8b360a736cf76ec1d0a2f654a2
Billal-MOKHTARI <billal.mokhtari@etu.uca.fr>
2024-06-06 16:28:06 +0200

    feat: Add positional encoding function for image processing

---
+++ models/networks/image_positional_encoding.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.fft import fft2, fftshift
+import numpy as np
+import math
+from tqdm import tqdm
--- src/maths.py
+++ src/maths.py
@@ -1,3 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.fft import fft2, fftshift
+import numpy as np
+import math
+from tqdm import tqdm
+import torch
+
 def required_kernel(in_size: int, out_size:int, stride=1, padding=1):
     assert in_size > 0, "Input size must be greater than 0"
     assert out_size > 0, "Output size must be greater than 0"
@@ -23,3 +31,35 @@ def get_output(in_size: int, kernel_size: int, stride=1, padding=1):
     assert padding >= 0, "Padding must be greater than or equal to 0"
     
     return (in_size-kernel_size+2*padding)//stride+1
+
+def pe(i, j, M, N, mask, factor=100):
+    phi = 1+(pow(-1, i)+pow(-1, j))/2
+    psi = 2-phi
+    ind_i = M*i+j
+    ind_j = N*j+i
+    theta = 2*math.pi*(i/pow(M, 2*ind_i/(factor*M*N))+j/pow(N, 2*ind_i/(factor*M*N)))+(i+j)/(M+N)**2
+
+
+    pe = (mask/4)*(phi*math.cos(theta)+psi*math.sin(theta))**2
+    return pe
+
+def image_pe(matrix):
+    if isinstance(matrix, torch.Tensor):
+        lib = "torch"
+        positional_matrix = torch.zeros_like(matrix)
+    else:
+        lib = "numpy"
+        positional_matrix = np.zeros_like(matrix)
+    
+    M, N = matrix.shape
+
+    for i in tqdm(range(M), desc="rows", unit="row"):
+        for j in range(N):
+            positional_matrix[i, j] = pe(i, j, M, N, matrix[i, j], lib)
+    
+    if lib == "torch":
+        positional_matrix /= torch.max(positional_matrix)
+    else:
+        positional_matrix /= np.max(positional_matrix)
+    
+    return positional_matrix
